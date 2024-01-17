8817a6edbc5aa3ebef6179fd603a522928158e76
João Macieira <jp.macieira@uol.com.br>
2024-01-17 11:41:31 -0300

    feat: crud gorm relations many to many

---
+++ 06-database/5-gorm-relations-many-to-many/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product `gorm:"many2many:products_categories;"`
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	Categories []Category `gorm:"many2many:products_categories;"`
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Category{}, &Product{})
+
+	// category := Category{Name: "Eletronicos"}
+	// db.Create(&category)
+	// category2 := Category{Name: "Cozinha"}
+	// db.Create(&category2)
+
+	// db.Create(&Product{
+	// 	Name:       "Tesoura",
+	// 	Price:      9.90,
+	// 	Categories: []Category{category, category2},
+	// })
+
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, c := range categories {
+		fmt.Println(c.Name, ":")
+
+		for _, p := range c.Products {
+			fmt.Println("- ", p.Name)
+		}
+	}
+}
