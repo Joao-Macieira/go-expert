b856f9253a41fb0229c94404e5f1b91a2e89ce8e
João Macieira <jp.macieira@uol.com.br>
2024-08-16 10:18:55 -0300

    feat: apis

---
+++ 09-apis/cmd/server/main.go
@@ -0,0 +1,92 @@
+package main
+
+// Fiber -> express
+// Go chi -> Router
+
+import (
+	"apis/configs"
+	"apis/internal/entity"
+	"apis/internal/infra/database"
+	"apis/internal/infra/webserver/handlers"
+	"log"
+	"net/http"
+
+	_ "apis/docs"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// @title           Go Expert API Example
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   João Macieira
+// @contact.url    http://www.joaomacieira.com.br
+// @contact.email  joao@macieira.com.br
+
+// @license.name   João Macieira License
+// @license.url    http://www.joaomacieira.com.br
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+func main() {
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProductDB(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUserDB(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", config.TokenAuth))
+	r.Use(middleware.WithValue("jwtExpiresIn", config.JWTExpiresIn))
+	r.Use(LogRequest)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Get("/", productHandler.GetAllProducts)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.CreateUser)
+	r.Post("/users/login", userHandler.GetJwt)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
+
+	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
