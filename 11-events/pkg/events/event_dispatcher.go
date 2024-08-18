ee4e080fef00bde8ff92a9fab978ddbc5fc3d49c
João Macieira <jp.macieira@uol.com.br>
2024-08-18 11:49:18 -0300

    feat: events

---
+++ 11-events/pkg/events/event_dispatcher.go
@@ -0,0 +1,67 @@
+package events
+
+import (
+	"errors"
+	"sync"
+)
+
+var ErrorHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{handlers: make(map[string][]EventHandlerInterface)}
+}
+
+func (e *EventDispatcher) Dispatch(event EventInterface) {
+	if handlers, ok := e.handlers[event.GetName()]; ok {
+		wg := &sync.WaitGroup{}
+		for _, handler := range handlers {
+			wg.Add(1)
+			go handler.Handle(event, wg)
+		}
+		wg.Wait()
+	}
+}
+
+func (e *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := e.handlers[eventName]; ok {
+		for _, h := range e.handlers[eventName] {
+			if h == handler {
+				return ErrorHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	e.handlers[eventName] = append(e.handlers[eventName], handler)
+	return nil
+}
+
+func (e *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, ok := e.handlers[eventName]; ok {
+		for _, h := range e.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (e *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := e.handlers[eventName]; ok {
+		for i, h := range e.handlers[eventName] {
+			if h == handler {
+				e.handlers[eventName] = append(e.handlers[eventName][:i], e.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+func (e *EventDispatcher) Clear() {
+	e.handlers = make(map[string][]EventHandlerInterface)
+}
